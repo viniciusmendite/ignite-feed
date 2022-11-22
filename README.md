0a043487514fa46f6a8c125de6875c0da1fd168c
Vinicius Mendite <vinicius@sharbe.com.br>
2022-11-21 21:29:08 -0300

    feat: publicação de posts

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center"><img src="https://github.com/viniciusmendite/ignite-feed/blob/master/assets/ignite-logo.svg" align="center" /> Ignite Feed</h1>
+
+Uma aplicação React utilizando o Vite e contendo os conceitos mais importantes do React, entre eles estão componentização, propriedades, estados, imutabilidade e hooks, além de ter o TypeScript no projeto para adicionar tipagem estática à aplicação.
+
+## 🚀 Começando
+
+Essas instruções permitirão que você obtenha uma cópia do projeto em operação na sua máquina local para fins de desenvolvimento e teste.
+
+### 📋 Pré-requisitos
+
+De que ferramentas preciso para executar a aplicação?
+
+```
+Git
+Node.js
+Yarn
+VS Code
+```
+
+### ⚙️ Executando
+
+Siga o passo-a-passo para ter um ambiente de desenvolvimento em execução.
+
+Clone o repositório:
+
+```
+git clone https://github.com/viniciusmendite/ignite-feed.git
+```
+
+Instale as dependências:
+
+```
+yarn
+```
+
+Execute a aplicação:
+
+```
+yarn dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
+
+import './global.css'
+import styles from './App.module.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/viniciusmendite.png',
+      name: 'Vinícius Mendite',
+      role: 'Front-end Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-11-09 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/danielhessell.png',
+      name: 'Daniel Hessel',
+      role: 'Back-end Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-11-11 20:00:00')
+  },
+]
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
